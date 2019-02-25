c4c0175a20c826d161a0bc47cfb00bb990628991
James McKeon <james.mckeon@mail.itsligo.ie>
2019-02-25 15:03:20 +0000

    Initial commit for week two branch. This is creating the framework for the two worksheets.

---
+++ Week2WS/Pages/WS1.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Week2WS.Pages
+{
+    public class WS1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
